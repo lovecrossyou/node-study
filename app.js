80ff9f33b4022a2fa5a99472afa2534aff0b57c9
love1234 <345841634@qq.com>
2018-10-30 13:54:35 +0800

    xx

--- app.js
+++ app.js
@@ -8,10 +8,11 @@ var http = require('http');
 var https = require('https');
 var fs = require('fs')
 
-// var options = {
-//     key:fs.readFileSync('xxx.key'),
-//     cert:fs.readFileSync('xxx.pem')
-// }
+var options = {
+    key:fs.readFileSync('/home/xituser/xiteng-server/software/ssl/214753937810730.key'),
+    cert:fs.readFileSync('/home/xituser/xiteng-server/software/ssl/214753937810730.pem')
+}
+
 var options = {}
 app.use(compression());
 app.disable('x-powered-by');
@@ -20,17 +21,15 @@ app.set('views', './dist'); // 指定视图所在的位置
 app.engine('html', ejs.__express);
 app.set('view engine', 'html');
 app.use(express.static(__dirname + "/dist/"));
-
 app.use('/xitenggamenode', proxys);
-
 
 app.get('/', function (req, res) {
     res.render('index')
 });
 
-var server = http.createServer(app).listen(3001, function () {
-    var host = server.address().address;
-    var port = server.address().port;
-    console.log('Example app listening at http://%s:%s', host, port);
-});
-// https.createServer(options,app).listen(3002);
+// var server = http.createServer(app).listen(3001, function () {
+//     var host = server.address().address;
+//     var port = server.address().port;
+//     console.log('Example app listening at http://%s:%s', host, port);
+// });
+https.createServer(options,app).listen(3002);
