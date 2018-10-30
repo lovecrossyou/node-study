e8960234f981c5db916ef2b40b4482469f5edfd2
love1234 <345841634@qq.com>
2018-10-30 13:42:51 +0800

    https配置

--- app.js
+++ app.js
@@ -4,29 +4,33 @@ var helmet = require('helmet')
 // Create our Express application
 var app = express();
 var proxys = require('./src/routes/proxys')
+var http = require('http');
+var https = require('https');
+var fs = require('fs')
 
+// var options = {
+//     key:fs.readFileSync('xxx.key'),
+//     cert:fs.readFileSync('xxx.pem')
+// }
+var options = {}
 app.use(compression());
 app.disable('x-powered-by');
 var ejs = require('ejs');  //我是新引入的ejs插件
-
-
 app.set('views', './dist'); // 指定视图所在的位置
-
 app.engine('html', ejs.__express);
-
 app.set('view engine', 'html');
 app.use(express.static(__dirname + "/dist/"));
 
-app.use('/xitenggamejar', proxys);
+app.use('/xitenggamenode', proxys);
 
 
 app.get('/', function (req, res) {
     res.render('index')
 });
 
-var server = app.listen(3001, function () {
+var server = http.createServer(app).listen(3001, function () {
     var host = server.address().address;
     var port = server.address().port;
-
     console.log('Example app listening at http://%s:%s', host, port);
 });
+// https.createServer(options,app).listen(3002);
--- src/routes/proxys.js
+++ src/routes/proxys.js
@@ -5,7 +5,7 @@ const proxy = require('http-proxy-middleware');
 
 // 代理服务
 router.use('/', proxy({
-    target: 'http://123.57.161.212:9939/',
+    target: 'https://www.xiteng.com/',
     changeOrigin: true,
 }));
 
