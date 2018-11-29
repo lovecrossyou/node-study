31c92f78d55356fce85f1dca44098c96147c7e48
love1234 <345841634@qq.com>
2018-11-29 09:53:58 +0800

    xx

--- app.js
+++ app.js
@@ -9,10 +9,10 @@ var fs = require('fs')
 var routers = require('./src/router')
 
 
-var options = {
-    key: fs.readFileSync('/home/xituser/xiteng-server/software/ssl/214753937810730.key'),
-    cert: fs.readFileSync('/home/xituser/xiteng-server/software/ssl/214753937810730.pem')
-}
+// var options = {
+//     key: fs.readFileSync('/home/xituser/xiteng-server/software/ssl/214753937810730.key'),
+//     cert: fs.readFileSync('/home/xituser/xiteng-server/software/ssl/214753937810730.pem')
+// }
 
 app.use(compression());
 app.disable('x-powered-by');
@@ -21,25 +21,25 @@ app.set('views', './views'); // 指定视图所在的位置
 //
 app.engine('html', ejs.__express);
 app.set('view engine', 'html');
-app.use( express.static(__dirname + "/views/"));
+app.use('/xitenggamenode', express.static(__dirname + "/views/"));
 
 app.use('/xitenggamejar', proxys);
 
 app.use('/xitenggamenode',routers)
 
 
-var server = https.createServer(options, app).listen(9934, function () {
-    var host = server.address().address;
-    var port = server.address().port;
-    console.log('app listening at http://%s%s', host, port);
-    console.log('app address', host, port);
-});
-
-
-// var server = http.createServer(app).listen(9934, function () {
+// var server = https.createServer(options, app).listen(9934, function () {
 //     var host = server.address().address;
 //     var port = server.address().port;
-//     console.log('ranning at host', host);
-//     console.log('ranning at port', port);
+//     console.log('app listening at http://%s%s', host, port);
+//     console.log('app address', host, port);
 // });
+
+
+var server = http.createServer(app).listen(9934, function () {
+    var host = server.address().address;
+    var port = server.address().port;
+    console.log('ranning at host', host);
+    console.log('ranning at port', port);
+});
 
