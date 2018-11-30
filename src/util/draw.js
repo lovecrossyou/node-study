e57799ef7e82adab18d978d9d776b9ac659f731b
love1234 <345841634@qq.com>
2018-11-30 18:57:26 +0800

    xxx

--- src/util/draw.js
+++ src/util/draw.js
@@ -51,9 +51,6 @@ const drawAvatar = async (ctx, icon) => {
     // await loadImage(path);
     const icon_xt = __dirname + '/../assets/logo_xiteng.png'
     const myimg = await loadImage(icon_xt);
-
-
-
     ctx.beginPath();
     ctx.arc(width_canvas / 2, 65, 35, 0, 2 * Math.PI);
     ctx.closePath();
