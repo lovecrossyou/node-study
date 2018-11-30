2b1a69ce820d47b6288fd788aea3c1e60c49096f
tianhaojie <1689177720@qq.com>
2018-11-30 16:50:55 +0800

    qrcode draw

--- src/controller/qrcode.js
+++ src/controller/qrcode.js
@@ -1,4 +1,6 @@
 const qr = require('qr-image')
+var QRCode = require('qrcode')
+const { Image ,createCanvas} = require('canvas')
 const path = require('path')
 const fs = require('fs')
 const {startDraw} = require('../util/draw')
@@ -16,9 +18,34 @@ exports.create_qrcode = function (req, res) {
         }
     })
     try {
-        var img = qr.image(text, {size: 10});
-        res.writeHead(200, {'Content-Type': 'image/png'});
-        img.pipe(res);
+
+        QRCode.toDataURL(text, { errorCorrectionLevel: 'H' ,scale:1, width:200, color: {
+                dark: '#e6454aff',  // Blue dots
+                light: '#ffffffff' // Transparent background
+            }},function (err, url) {
+            const img = new Image()
+            img.onload = () =>{
+
+
+
+                const canvas = createCanvas(200, 200);
+                const ctx = canvas.getContext('2d')
+                ctx.drawImage(img, 0, 0)
+
+                const stream = canvas.createPNGStream()
+                stream.pipe(res)
+                // res.writeHead(200, {'Content-Type': 'image/png'});
+                // img.pipe(res);
+            }
+            img.onerror = err => { throw err }
+            img.src = url;
+
+            console.log('thj+======',url)
+            // var img = qr.image(text, {size: 10});
+
+
+        })
+
     } catch (e) {
         res.writeHead(414, {'Content-Type': 'text/html'});
         res.end('<h1>414 Request-URI Too Large</h1>');
--- src/util/draw.js
+++ src/util/draw.js
@@ -1,13 +1,15 @@
 const { createCanvas,Image,loadImage,registerFont } = require('canvas')
 const querystring = require('querystring');
-
+const width_canvas = 670;
+const height_canvas = 940;
+var desHeight = 100;
 
 registerFont(__dirname + '/PINGFANG.TTF', {family: '苹方'});
 
 //https://www.xiteng.com/xitenggamenode/createShareImg?discountGameId=1913&inviteId=7&userName=
 const startDraw = async (req,res) => {
 
-    const {userName,logo,inviteId,discountGameId} = req.query ;
+    const {userName,logo,inviteId,discountGameId,des} = req.query ;
 
     const qrTextPretty = 'https://www.xiteng.com/xitenggamenode/create_qrcode?text=https://www.xiteng.com/xitenggamejar/index?discountGameId='+discountGameId+'&inviteId='+inviteId
 
@@ -18,18 +20,21 @@ const startDraw = async (req,res) => {
     console.log('logo ',avatarLogo);
 
     // 创建画布
-    const canvas = createCanvas(570, 940);
+    const canvas = createCanvas(width_canvas, height_canvas);
     const ctx = canvas.getContext('2d')
-    ctx.fillStyle = '#fff';
-    ctx.fillRect(0,0,570,960);
+    await DrawImage(ctx,__dirname + '/../assets/share/bg_photo_yaoqing1.png',{x:0,y:0,w:width_canvas,h:height_canvas})
+    drawRoundRect(ctx, (width_canvas-596)/2, 56, 596, 810, 10);
 
     if(userName){
         const nameStr = userName.split('__')[0]
         drawName(ctx, nameStr||'');
     }
-    drawDes(ctx, '亲，一起来免费抽签抢金条吧！', 0);
-    drawDes(ctx, '下一条锦鲤就是你的', 1);
-    await drawJinLi(ctx, '');
+    let desStr = '亲，一起来免费抽签抢金条吧！下一条锦鲤就是你!!!'
+    if (des){
+        desStr = des.split('__')[0]
+    }
+    drawDes(ctx, desStr, 0);
+    await DrawImage(ctx,__dirname + '/../assets/share/fenxiang_xiaocehngxu.png',{x:(width_canvas-504)/2,y:190+desHeight+30,w:504,h:416})
     drawcodeDes(ctx, '运势来袭，下一条锦鲤就是你的！', 0);
     drawcodeDes(ctx, '长按识别小程序，立即加入抢购', 1);
     if(qrTextPretty){
@@ -37,18 +42,23 @@ const startDraw = async (req,res) => {
     }
     await drawZeroImg(ctx, '');
     if(avatarLogo){
-        await drawAvatar(ctx, avatarLogo);
+        await drawAvatar(ctx,avatarLogo);
     }
 
     const stream = canvas.createPNGStream()
     stream.pipe(res)
 }
-
+const DrawImage = async (ctx,path,react)=>{
+    const myimg = await loadImage(path);
+    ctx.drawImage(myimg, react.x, react.y, react.w, react.h);
+}
 const drawAvatar = async (ctx, icon) => {
     const myimg = await loadImage(icon);
-    ctx.arc(570 / 2, 65, 35, 0, 2 * Math.PI);
+    ctx.beginPath();
+    ctx.arc(width_canvas / 2, 65, 35, 0, 2 * Math.PI);
+    ctx.closePath();
     ctx.clip();
-    ctx.drawImage(myimg, 570 / 2 - 35, 30, 70, 70);
+    ctx.drawImage(myimg, width_canvas / 2 - 35, 30, 70, 70);
     ctx.restore()
 }
 
@@ -59,47 +69,72 @@ const drawName = (ctx, name) => {
     ctx.fillStyle = "#333333";
     // ctx.textAlign='center'
     ctx.font = "24px 苹方";
-    ctx.fillText(text, (570 - ctx.measureText(text).width) / 2, 140);
+    ctx.fillText(text, (width_canvas - ctx.measureText(text).width) / 2, 140);
 }
 
 const drawDes = (ctx, des, index) => {
     ctx.beginPath();
-    var text = des;
-    //字体大小,类型
     ctx.fillStyle = "#333333";
     ctx.font = "26px 苹方";
-    ctx.fillText(text, (570 - ctx.measureText(text).width) / 2, 190 + 30 * index);
+    ctx.lineWidth=1;
+    var str = des
+    var lineWidth = 0;
+    var canvasWidth = 450;//计算canvas的宽度
+    var initHeight=0;//绘制字体距离canvas顶部初始的高度
+    var lastSubStrIndex= 0; //每次开始截取的字符串的索引
+    if (str.length>50){
+        str = str.substr(0,50)
+    }
+    for(let i=0;i<str.length;i++){
+
+        lineWidth+=ctx.measureText(str[i]).width;
+        if(lineWidth>canvasWidth){
+            ctx.fillText(str.substring(lastSubStrIndex,i),110,initHeight+190);//绘制截取部分
+            initHeight+=35;//20为字体的高度
+            lineWidth=0;
+            lastSubStrIndex=i;
+        }
+        if(i==str.length-1){//绘制剩余部分
+            ctx.fillText(str.substring(lastSubStrIndex,i+1),100,initHeight+190);
+            console.log(initHeight)
+        }
+        desHeight = initHeight;
+
+    }
 
 }
-
-const drawJinLi = async (ctx, jinli) => {
-    const avatar = __dirname + '/../assets/fenxiang_xiaocehngxu.png'
-    const myimg = await loadImage(avatar);
-    ctx.drawImage(myimg, (570 - 480) / 2, 250, 480, 440);
-}
-
 const drawcodeDes = (ctx, des, index) => {
     ctx.beginPath();
     var text = des;
     //字体大小,类型
     ctx.fillStyle = "#e6454a";
     ctx.font = "24px 苹方";
-    ctx.fillText(text, (570 - 480) / 2, 770 + 28 * index)
-
+    ctx.fillText(text, (width_canvas - 480) / 2, 770 + 28 * index)
 }
-
 const drawQR = async (ctx, qrText) => {
-    const myimg = await loadImage(qrText);
-    ctx.drawImage(myimg, 400, 715, 120, 120);
+    await DrawImage(ctx,qrText,{x:460,y:715,w:125,h:125})
+    await DrawImage(ctx,__dirname + '/../assets/share/logo_fenxiang_qianggou.png',{x:460+(125-35)/2,y:715+(125-35)/2,w:35,h:35})
 }
 
 const drawZeroImg = async (ctx, jinli) => {
-    const avatar = __dirname + '/../assets/icon_0yuanqiang.png'
-    const myimg = await loadImage(avatar);
-    ctx.drawImage(myimg, (570 - 127) / 2, 860, 127, 27);
+    await DrawImage(ctx,__dirname + '/../assets/share/logo_fenxiang_qianggou1.png',{x:(width_canvas - 155) / 2,y:890,w:155,h:35})
 }
+const drawRoundRect =(cxt, x, y, width, height, radius)=>{
+
+    cxt.beginPath();
+    cxt.arc(x + radius, y + radius, radius, Math.PI, Math.PI * 3 / 2);
+    cxt.lineTo(width - radius + x, y);
+    cxt.arc(width - radius + x, radius + y, radius, Math.PI * 3 / 2, Math.PI * 2);
+    cxt.lineTo(width + x, height + y - radius);
+    cxt.arc(width - radius + x, height - radius + y, radius, 0, Math.PI * 1 / 2);
+    cxt.lineTo(radius + x, height +y);
+    cxt.arc(radius + x, height - radius + y, radius, Math.PI * 1 / 2, Math.PI);
+    cxt.closePath();
+    cxt.fillStyle = "#fff";
+    cxt.fill();
 
 
+}
  module.exports= {
     startDraw
  }
