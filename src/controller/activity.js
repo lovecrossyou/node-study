2516c6f27a891914a0c3f7bff2b656467e64986e
love1234 <345841634@qq.com>
2019-02-01 19:30:25 +0800

    xx

--- src/controller/activity.js
+++ src/controller/activity.js
@@ -8,31 +8,34 @@
 
 exports.activityInfo = function (req, res) {
     const info =  {
-        "invite_group": {
-          "picture": "http://qnimage.xiteng.com/invite_link@2x.png",
-          "text": "猪年大吉，金猪送福，抽签抢苹果电脑，抽中即送，祝你好运！"
-        },
-        "me_invite_friend": {
-          "input_text": "猪年大吉，金猪送福，抽签抢苹果笔记本电脑，抽中即送，公开透明，祝你好运！",
-          "picture": "http://qnimage.xiteng.com/me_invite_friend@2x.png",
+      "invite_group": {
+          "picture": "http://qnimage.xiteng.com/%E9%82%80%E8%AF%B7%E9%93%BE%E6%8E%A5@2x.png",
+          "text": "猪年大吉，金猪送福，参加3D抢购！中签即送，立即抢！"
+      },
+      "me_invite_friend": {
+          "input_text": "猪年大吉，金猪送福，一起来参加3D抢购！中签即送，公开透明，立即抢！",
+          "picture": "http://qnimage.xiteng.com/%E6%88%91%E9%82%80%E8%AF%B7%E5%A5%BD%E5%8F%8B@2x.png",
           "invite_link": {
-            "picture": "http://qnimage.xiteng.com/invite_link@2x.png",
-            "text": "猪年大吉，金猪送福，抽签抢苹果电脑，抽中即送，祝你好运！"
+              "picture": "http://qnimage.xiteng.com/%E9%82%80%E8%AF%B7%E9%93%BE%E6%8E%A5@2x.png",
+              "text": "猪年大吉，金猪送福，参加3D抢购！中签即送，立即抢！"
           },
           "invite_info": {
-            "picture": "http://qnimage.xiteng.com/invite_picture@2x.png",
-            "text": "猪年大吉，金猪送福，抽签抢苹果笔记本电脑，抽中即送，公开透明，祝你好运！"
+              "picture": "http://qnimage.xiteng.com/%E9%82%80%E8%AF%B7%E7%94%9F%E6%88%90%E5%9B%BE-%E8%8D%A3%E8%80%80@2x.png",
+              "text": "猪年大吉，金猪送福，一起来参加3D抢购！中签即送，公开透明，立即抢！"
           }
-        },
-        "rule": {
+      },
+      "rule": {
           "zero_picture": "http://qnimage.xiteng.com/%E8%A7%84%E5%88%99-%E6%8A%BD%E7%AD%BE%E6%8A%A2.jpg",
-          "common_picture": "http://qnimage.xiteng.com/%E5%96%9C%E8%85%BE%E6%8A%A2%E8%B4%AD%E6%B4%BB%E5%8A%A8%E8%A7%84%E5%88%99.jpg",
-        },
-        "awards": {
-          "picture": "http://qnimage.xiteng.com/photo_xiangqing@2x%281%29.jpg",
-        },
-        "home_activity_big_picture": "http://qnimage.xiteng.com/%E8%8B%B9%E6%9E%9C%E7%94%B5%E8%84%91%E9%A6%96%E9%A1%B5.jpg"
-      }
+          "common_picture": "http://qnimage.xiteng.com/%E5%96%9C%E8%85%BE%E6%8A%A2%E8%B4%AD%E6%B4%BB%E5%8A%A8%E8%A7%84%E5%88%99.jpg"
+      },
+      "awards": {
+          "picture": "http://qnimage.xiteng.com/photo_xiangqing@2x%281%29.jpg"
+      },
+      "home_activity_big_picture": "http://qnimage.xiteng.com/%E8%8B%B9%E6%9E%9C%E7%94%B5%E8%84%91%E9%A6%96%E9%A1%B5.jpg",
+      "zerobg": "http://qnimage.xiteng.com/photo_bg_chouqian@2x.png",
+      "threed_spectial": "http://qnimage.xiteng.com/icon_3Drexiao@2x.png",
+      "threed_header": "http://qnimage.xiteng.com/icon_3D_qianggou@2x.png"
+  }
 
     res.send(info);
 }
