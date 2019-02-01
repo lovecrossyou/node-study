f010262516f7cacecf88cd55f290462e1540c37b
love1234 <345841634@qq.com>
2019-02-01 11:25:50 +0800

    xx

--- src/controller/activity.js
+++ src/controller/activity.js
@@ -1,4 +1,3 @@
-// 1、首页-抽签抢购：大图
 // 2、抽签抢购邀请拼团链接：图片、文字
 // 3、我-邀请好友：
 // （1）图片、输入文字  
@@ -7,8 +6,33 @@
 // 4、奖品详情：中签榜-抽签抢-中签详情-奖品详情：图片
 // 5、首页-抢购规则：图片、抽签抢商品详情规则、图片
 
-exports.activityInfo = function (req,res) {
-    return {
-
+exports.activityInfo = function (req, res) {
+    const info =  {
+        "invite_group": {
+            "picture": "",
+            "text": ""
+        },
+        "me_invite_friend": {
+            "input_text": "",
+            "picture": "http://qnimage.xiteng.com/me_invite_friend@2x%20.png",
+            "invite_link": {
+                "picture": "",
+                "text": ""
+            },
+            "invite_info": {
+                "picture": "",
+                "text": ""
+            }
+        },
+        "rule": {
+            "zero_picture": "",
+            "common_picture": ""
+        },
+        "awards": {
+            "picture": ""
+        },
+        "home_activity_big_picture": ""
     }
+
+    res.send(info);
 }
