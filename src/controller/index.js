cd7a3a5faa150673ad4f4adddec8c38513a54831
love1234 <345841634@qq.com>
2019-02-01 11:32:37 +0800

    xx

--- src/controller/index.js
+++ src/controller/index.js
@@ -1,8 +1,10 @@
 var qiniu = require('./qiniu');
 var qrcode = require('./qrcode')
 var shareInfo = require('./shareInfo')
+var activity = require('./activity')
 module.exports = {
     qiniu:qiniu,
     qrcode:qrcode,
     shareInfo:shareInfo,
+    activity:activity
 }
