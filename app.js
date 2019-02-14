ecec9e73ba577869e0d25d20dd0c3e3961ae5a73
love1234 <345841634@qq.com>
2019-02-14 11:22:41 +0800

    xx

--- app.js
+++ app.js
@@ -18,14 +18,14 @@ app.use('/xitenggamejar', proxys);
 app.use('/xitenggamenode',routers)
 
 
-var allowCrossDomain = function(req, res, next) {
-    res.header('Access-Control-Allow-Origin', 'http://localhost:9934');
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-    res.header('Access-Control-Allow-Headers', 'Content-Type');
-    // res.header('Access-Control-Allow-Credentials','true');
-    next();
-};
-app.use(allowCrossDomain);
+// var allowCrossDomain = function(req, res, next) {
+//     res.header('Access-Control-Allow-Origin', 'http://localhost:9934');
+//     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+//     res.header('Access-Control-Allow-Headers', 'Content-Type');
+//     res.header('Access-Control-Allow-Credentials','true');
+//     next();
+// };
+// app.use(allowCrossDomain);
 
 
 
