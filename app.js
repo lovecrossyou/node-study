adf07e331ba6ac47b6632e5fb74d250178768345
love1234 <345841634@qq.com>
2019-02-14 13:28:13 +0800

    xx

--- app.js
+++ app.js
@@ -1,18 +1,20 @@
 var express = require('express');
 var compression = require('compression');
+var cors = require('cors');
 // Create our Express application
 var app = express();
 var proxys = require('./src/proxy/proxys')
 var http = require('http');
 var routers = require('./src/router')
 
-var allowCrossDomain = function(req, res, next) {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-    res.header('Access-Control-Allow-Headers', 'Content-Type');
-    next();
-};
-app.use(allowCrossDomain);
+// var allowCrossDomain = function(req, res, next) {
+//     res.header('Access-Control-Allow-Origin', '*');
+//     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+//     res.header('Access-Control-Allow-Headers', 'Content-Type');
+//     next();
+// };
+// app.use(allowCrossDomain);
+app.use(cors());
 app.use(compression());
 app.disable('x-powered-by');
 var ejs = require('ejs');  //我是新引入的ejs插件
